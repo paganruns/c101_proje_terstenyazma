7ee5936e2feb895ece766b94861898db981673d2
SahinZambak <sahinzambak7@gmail.com>
2022-08-02 16:48:16 +0300

    222

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir şeyler yazınız.");
+            string metin = Console.ReadLine();
+            string[] Metin = metin.Split(' ');
+
+            foreach (string item in Metin)
+            {
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(item[i]);
+                }
+                Console.Write(" ");
+            }
+    }
+}
